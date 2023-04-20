56339f269c756855bef72cb2664da2cab316aa1f
Seven <graf.lancelot7@gmail.com>
2023-04-20 14:40:19 +0300

    summary parcel update

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import {str, getAllId, getAllUsers} from "./js/validate-password";
+console.log( str);
+console.log( getAllId);
+console.log( getAllUsers);
---
+++ src/js/validate-password.js
@@ -0,0 +1,9 @@
+export const str = "it's work!";
+
+export function getAllUsers() {
+  console.log("All Users");
+}
+
+export function getAllId() {
+  console.log("All id");
+}
